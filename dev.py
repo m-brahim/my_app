5497c4536610dfe4b17f5a0eb484a92a0fb84905
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-07 11:44:58 +0100

    Update dev.py

--- dev.py
+++ dev.py
@@ -186,7 +186,7 @@ if selection_effectuee:
         fig_gauge.update_layout(
             height=200,
             font=dict(size=16),
-            margin=dict(l=10, r=10, t=50, b=10, pad=8),
+            margin=dict(l=10, r=10, t=20, b=10, pad=8),
         )
 
         # Affichage de la jauge sous le tableau existant
