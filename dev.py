49085dff170e6cfcf9279532358586bf178160fe
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-07 11:23:58 +0100

    Update dev.py

--- dev.py
+++ dev.py
@@ -588,7 +588,7 @@ clients_by_country = df.drop_duplicates(subset=['ID client', 'Pays/Région']).gr
 merged_data = pd.merge(data_f, clients_by_country, how='left', on='Pays/Région')
 
 # icône personnalisée pour représenter un client (ici l'exemple c'est Kiloutou)
-icon_path = 'pages/Kiloutou_logo.jpg'
+icon_path = 'Kiloutou_logo.jpg'
 client_icon = folium.CustomIcon(icon_image=icon_path, icon_size=(20, 20))
 
 if selection:
