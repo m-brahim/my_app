eb2322ba9e770ba1d1479dcb38c2cf1fc15ec62b
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-13 10:48:23 +0100

    Update dev.py

--- dev.py
+++ dev.py
@@ -609,7 +609,7 @@ client_icon = folium.CustomIcon(icon_image=icon_path, icon_size=(20, 20))
 
 if selection:
     # définition d'une localisation initiale
-    my_map = folium.Map(location=[merged_data['Latitude'].iloc[0], merged_data['Longitude'].iloc[0]], zoom_start=5)
+    my_map = folium.Map(location=[merged_data['Latitude'].iloc[0], merged_data['Longitude'].iloc[0]], zoom_start=5.5)
 
     # ajoutez un seul marqueur pour représenter le pays avec le nombre de clients dans l'infobulle
     folium.Marker([merged_data['Latitude'].iloc[0], merged_data['Longitude'].iloc[0]],
