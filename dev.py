7c7d53c784865256fb171741738fa0d5271fa5c1
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-14 11:02:25 +0100

    Update dev.py

--- dev.py
+++ dev.py
@@ -711,6 +711,7 @@ if selected3 == "Import":
         dfo['Prévision des ventes'] = dfo['Prévision des ventes'].astype(str)
         dfo['Prévision des ventes'] = dfo['Prévision des ventes'].str.replace('[^\d]', '', regex=True)
         dfo['Prévision des ventes'] = pd.to_numeric(dfo['Prévision des ventes'])
+        dfo['Prévision des ventes'] = dfo['Prévision des ventes'].applymap(lambda x: "%d" % x)
 
         total_sales = dfo['Prévision des ventes'].sum()
 
