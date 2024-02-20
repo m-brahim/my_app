31752c521b6debe9adae93cdb29cb7144717072d
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-20 11:26:11 +0100

    Update dev.py

--- dev.py
+++ dev.py
@@ -992,7 +992,7 @@ if selected3 == "Tâches" :
         {"Tâches" : "Intégration des données","Personnes Assignées" : "2", "Durée": "4h", "Etat": "en cours", "Durée restante" : "2h"},
                             ])
 
-    edited_df = st.data_editor(tasks_df, width=1426, height=500,num_rows="dynamic")    
+    edited_df = st.data_editor(tasks_df, width=1426, height=600,num_rows="dynamic")    
 
 
 
