72b1c4c53ab21844a05c6259f93177e38f275cb4
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-21 17:00:45 +0100

    Update dev.py

--- dev.py
+++ dev.py
@@ -1091,26 +1091,11 @@ if selected3 == "Tests":
 
 
 if selected3 == "Elements":
-    with elements("dashboard"):
-        layout = [
-            dashboard.Item("first_item", 0, 0, 2, 2),
-            dashboard.Item("second_item", 2, 0, 2, 2, isDraggable=False, moved=False),
-            dashboard.Item("third_item", 0, 2, 1, 1, isResizable=False),
-        ]
-
-    with dashboard.Grid(layout):
-        mui.Paper("First item", key="first_item")
-        mui.Paper("Second item (cannot drag)", key="second_item")
-        mui.Paper("Third item (cannot resize)", key="third_item")
-
-    def handle_layout_change(updated_layout):
-        # You can save the layout in a file, or do anything you want with it.
-        # You can pass it back to dashboard.Grid() if you want to restore a saved layout.
-        print(updated_layout)
-
-    with dashboard.Grid(layout, onLayoutChange=handle_layout_change):
-        mui.Paper("First item", key="first_item")
-        mui.Paper("Second item (cannot drag)", key="second_item")
-        mui.Paper("Third item (cannot resize)", key="third_item")
+    with elements("multiple_children"):
+        mui.Button(
+            mui.icon.EmojiPeople,
+            mui.icon.DoubleArrow,
+            "Button with multiple children"
+        )
 
     
