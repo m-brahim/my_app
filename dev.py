244bd2cde9c3dc30fbdeea3e88b95a140c973a2c
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-22 09:26:55 +0100

    Update dev.py

--- dev.py
+++ dev.py
@@ -1094,21 +1094,17 @@ if selected3 == "Elements":
     with elements("dashboard"):
         from streamlit_elements import dashboard
         layout = [
-            dashboard.Item("first_item", 0, 0, 2, 2),
-            dashboard.Item("second_item", 2, 0, 2, 2, isDraggable=False, moved=False),
-            dashboard.Item("third_item", 0, 2, 1, 1, isResizable=False),
+            dashboard.Item("graphique", 0, 0, 2, 2),
+            dashboard.Item("metric", 0, 0, 2, 2),
+            dashboard.Item("tableau", 0, 0, 2, 2),
         ]
-        with dashboard.Grid(layout):
-            mui.Paper("First item", key="first_item")
-            mui.Paper("Second item (cannot drag)", key="second_item")
-            mui.Paper("Third item (cannot resize)", key="third_item")
-
+        
         def handle_layout_change(updated_layout):
             # You can save the layout in a file, or do anything you want with it.
             # You can pass it back to dashboard.Grid() if you want to restore a saved layout.
             print(updated_layout)
 
         with dashboard.Grid(layout, onLayoutChange=handle_layout_change):
-            mui.Paper("First item", key="first_item")
-            mui.Paper("Second item (cannot drag)", key="second_item")
-            mui.Paper("Third item (cannot resize)", key="third_item")
+            mui.Paper("graphique", key="graphique")
+            mui.Paper("metric", key="metric")
+            mui.Paper("tableau", key="tableau")
