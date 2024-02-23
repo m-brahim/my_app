91304b0207ca58db044e41f154d3bd761d46bec3
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-23 17:55:42 +0100

    Update dev.py

--- dev.py
+++ dev.py
@@ -1193,8 +1193,8 @@ if selected3 == "Elements" :
     with elements("nivo_pie"):
         
         from streamlit_elements import nivo
-        from nivo import patternDotsDef 
-        from nivo import patternSquaresDef 
+        from nivo.core import patternDotsDef 
+        from nivo.core import patternSquaresDef 
 
         data2 = [{"id": "go","label": "go","value": 265,"color": "hsl(351, 70%, 50%)"},
                  {"id": "scala","label": "scala","value": 205, "color": "hsl(175, 70%, 50%)"},
