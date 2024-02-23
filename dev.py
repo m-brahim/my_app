6c355d5edbac2acf68c40ebe258fb43ce5208d31
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-23 16:25:29 +0100

    Update dev.py

--- dev.py
+++ dev.py
@@ -1220,7 +1220,7 @@ if selected3 == "Elements" :
                 arcLabelsSkipAngle=10,
                 arcLabelsTextColor={"from" : "color", "modifiers" : [["darker", 2]]},
                 
-                defs=[{"id" : "dots", "type" : "patternDots", "background" : "inherit", "color" : "rgba(255, 255, 255, 0.3)", "size" : 4, "padding" : 1, "stagger" : true},
+                defs=[{"id" : "dots", "type" : "patternDots", "background" : "inherit", "color" : "rgba(255, 255, 255, 0.3)", "size" : 4, "padding" : 1},
                 {"id" : "lines", "type" : "patternLines", "background" : "inherit", "color" : "rgba(255, 255, 255, 0.3)", rotation: -45, lineWidth: 6, spacing: 10}],
             
                 fill=[{"match" : {"id": "ruby"}, "id" : "dots"}, 
