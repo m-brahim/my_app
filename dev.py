89b31e6b7a5f791bbfd2b5124c3daf36a70587bf
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-23 16:20:27 +0100

    Update dev.py

--- dev.py
+++ dev.py
@@ -1191,21 +1191,80 @@ if selected3 == "Elements" :
                 }
             )
 
+    
+    with elements("nivo_pie"):
+        from streamlit_elements import nivo
 
+        data2 = [{"id": "go","label": "go","value": 265,"color": "hsl(351, 70%, 50%)"},
+                 {"id": "scala","label": "scala","value": 205, "color": "hsl(175, 70%, 50%)"},
+                 {"id": "css","label": "css","value": 51,"color": "hsl(341, 70%, 50%)"},
+                 {"id": "javascript","label": "javascript","value": 130,"color": "hsl(349, 70%, 50%)"},
+                 {"id": "erlang","label": "erlang","value": 292,"color": "hsl(342, 70%, 50%)"}]
 
-        
-
-
-
-
-
-
-
-
-
-
-
-
-        
+        with mui.Box(sx={"height": 500}):
+            nivo.Pie(
+                data=data2,
+                keys=[ "go", "scala", "css", "javascript", "erlang"],
+                margin={ "top": 40, "right": 80, "bottom": 80, "left": 80 },
+                innerRadius=0.5,
+                padAngle=0.7,
+                cornerRadius=3,
+                activeOuterRadiusOffset=8,
+                borderWidth=1,
+                borderColor={"from": "color", "modifiers" : [["darker", 0.2]]},
+                
+                arcLinkLabelsSkipAngle=10,
+                arcLinkLabelsTextColor="#333333",
+                arcLinkLabelsThickness=2,
+                arcLinkLabelsColor={ "from": "color" },
+                arcLabelsSkipAngle=10,
+                arcLabelsTextColor={"from" : "color", "modifiers" : [["darker", 2]]}
+                
+                defs=[{"id" : "dots", "type" : "patternDots", "background" : "inherit", "color" : "rgba(255, 255, 255, 0.3)", "size" : 4, "padding" : 1, "stagger" : true},
+                {"id" : "lines", "type" : "patternLines", "background" : "inherit", "color" : "rgba(255, 255, 255, 0.3)", rotation: -45, lineWidth: 6, spacing: 10}]
+            
+                fill=[{"match" : {"id": "ruby"}, "id" : "dots"}, 
+                      {"match" : {"id": "c"}, "id": "dots"}, 
+                      {"match": {"id": "go"}, "id": "dots"},
+                      {"match" : {"id" : "python"},"id": "dots"},
+                      {"match" : {"id" : "scala"}, "id": "lines"},
+                      {"match" : {"id" : "lisp"}, "id" : "lines"},
+                      {"match": {"id" : "elixir"}, "id": "lines"},
+                      {"match" : {"id" : "javascript"}, "id": "lines"}]
+                
+                legends=[{
+                    "anchor": "bottom",
+                    "direction" : "row",
+                    "justify" : false,
+                    "translateX" : 0,
+                    "translateY" : 56,
+                    "itemsSpacing": 0,
+                    "itemWidth" : 100,
+                    "itemHeight" : 18,
+                    "itemTextColor" : "#999",
+                    "itemDirection" : "left-to-right",
+                    "itemOpacity" : 1,
+                    "symbolSize" : 18,
+                    "symbolShape" : "circle",
+                    "effects" : [
+                        {
+                            "on" : "hover",
+                            "style"  : {
+                            "itemTextColor" : "#000"
+                            }
+                        }
+                    ]
+                }
+                        ]
+            )
     
     
+    
+    
+    
+    
+    
+    
+            
+        
+        
