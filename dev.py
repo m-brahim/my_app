adc9a9db2ca477b21d790a785250479e37fe8092
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-27 11:36:02 +0100

    Update dev.py

--- dev.py
+++ dev.py
@@ -1262,48 +1262,44 @@ if selected3 == "Elements" :
             )
 
 
-    layout = [dashboard.Item("bar_chart", 0, 0, 4, 3),  # Positionnez votre graphique en barres ici]
 
-    # Créez votre dashboard avec le layout défini
-    dashboard_grid = dashboard.Grid(layout)
-	      
-    with mui.Paper(key="bar_chart"):
-	    # Insérez votre code de graphique ici
-	    nivo.Bar(
-	        data=data3,
-	        keys=['hot dog', 'burger', 'sandwich', 'kebab', 'donut'],
-	        indexBy="country",
-	        margin={"top": 50, "right": 130, "bottom": 50, "left": 60},
-	        padding=0.3,
-	        valueScale={"type": "linear"},
-	        indexScale={"type": "band"},
-	        colors={"scheme": "nivo"},
-	        borderColor={"from": "color", "modifiers": [["darker", 1.6]]},
-	        axisBottom={"tickSize": 5, "tickPadding": 5, "tickRotation": 0, "legend": "country",
-	                    "legendPosition": "middle", "legendOffset": 32, "truncateTickAt": 0},
-	        axisLeft={"tickSize": 5, "tickPadding": 5, "tickRotation": 0, "legend": "food",
-	                  "legendPosition": "middle", "legendOffset": -40, "truncateTickAt": 0},
-	        labelSkipWidth=12,
-	        labelSkipHeight=12,
-	        labelTextColor={"from": "color", "modifiers": [["darker", 1.6]]},
-	        legends=[{
-	            "dataFrom": "keys",
-	            "anchor": "bottom-right",
-	            "direction": "column",
-	            "translateX": 120,
-	            "translateY": 0,
-	            "itemsSpacing": 2,
-	            "itemWidth": 100,
-	            "itemHeight": 20,
-	            "itemDirection": "left-to-right",
-	            "itemOpacity": 0.85,
-	            "symbolSize": 20,
-	            "effects": [{"on": "hover", "style": {"itemOpacity": 1}}]
-	        }]
-	    )
-        
+    with elements("dashboard"):
+	    layout = dashboard.Item("bar_chart", 0, 0, 4, 3)
     
-    
+    with dashboard.Grid(layout):
+	    with mui.Paper(key="bar_chart"):
+		    nivo.Bar(
+			    data=data3,
+		            keys=['hot dog', 'burger', 'sandwich', 'kebab', 'donut'],
+		            indexBy="country",
+		            margin={"top": 50, "right": 130, "bottom": 50, "left": 60},
+		            padding=0.3,
+		            valueScale={"type": "linear"},
+		            indexScale={"type": "band"},
+		            colors={"scheme": "nivo"},
+		            borderColor={"from": "color", "modifiers": [["darker", 1.6]]},
+		            axisBottom={"tickSize": 5, "tickPadding": 5, "tickRotation": 0, "legend": "country",
+		                        "legendPosition": "middle", "legendOffset": 32, "truncateTickAt": 0},
+		            axisLeft={"tickSize": 5, "tickPadding": 5, "tickRotation": 0, "legend": "food",
+		                      "legendPosition": "middle", "legendOffset": -40, "truncateTickAt": 0},
+		            labelSkipWidth=12,
+		            labelSkipHeight=12,
+		            labelTextColor={"from": "color", "modifiers": [["darker", 1.6]]},
+		            legends=[{
+		                "dataFrom": "keys",
+		                "anchor": "bottom-right",
+		                "direction": "column",
+		                "translateX": 120,
+		                "translateY": 0,
+		                "itemsSpacing": 2,
+		                "itemWidth": 100,
+		                "itemHeight": 20,
+		                "itemDirection": "left-to-right",
+		                "itemOpacity": 0.85,
+		                "symbolSize": 20,
+		                "effects": [{"on": "hover", "style": {"itemOpacity": 1}}]
+		            }]
+		        )
             
         
         
