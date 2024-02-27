40b4f5b3914055ffbdffc0eef397117df4e5ffe1
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-27 16:11:05 +0100

    Update dev.py

--- dev.py
+++ dev.py
@@ -1189,7 +1189,7 @@ if selected3 == "Elements" :
 
 
 			with mui.Paper(key="third_item"):
-				with mui.AppBar(position="static"):
+				mui.AppBar(position="static")
 					
 				nivo.Bar(
 			                data=data3,
