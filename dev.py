7ff8cb91ef2b89b2e0a708dc3502683d9125bb9d
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-27 15:05:18 +0100

    Update dev.py

--- dev.py
+++ dev.py
@@ -1097,61 +1097,42 @@ if selected3 == "Tests":
 
 
 if selected3 == "Elements" :   
-    with elements("nivo_radar"):
-    
-        DATA = [
-            { "taste": "fruity", "chardonay": 93, "carmenere": 61, "syrah": 114 },
-            { "taste": "bitter", "chardonay": 91, "carmenere": 37, "syrah": 72 },
-            { "taste": "heavy", "chardonay": 56, "carmenere": 95, "syrah": 99 },
-            { "taste": "strong", "chardonay": 64, "carmenere": 90, "syrah": 30 },
-            { "taste": "sunny", "chardonay": 119, "carmenere": 94, "syrah": 103 },
-        ]
-    
-        with mui.Box(sx={"height": 500}):
-            nivo.Radar(
-                data=DATA,
-                keys=[ "chardonay", "carmenere", "syrah" ],
-                indexBy="taste",
-                valueFormat=">-.2f",
-                margin={ "top": 70, "right": 80, "bottom": 40, "left": 80 },
-                borderColor={ "from": "color" },
-                gridLabelOffset=36,
-                dotSize=10,
-                dotColor={ "theme": "background" },
-                dotBorderWidth=2,
-                motionConfig="wobbly",
-                legends=[
-                    {
-                        "anchor": "top-left",
-                        "direction": "column",
-                        "translateX": -50,
-                        "translateY": -40,
-                        "itemWidth": 80,
-                        "itemHeight": 20,
-                        "itemTextColor": "#999",
-                        "symbolSize": 12,
-                        "symbolShape": "circle",
-                        "effects": [
-                            {
-                                "on": "hover",
-                                "style": {
-                                    "itemTextColor": "#000"
-                                }
-                            }
-                        ]
-                    }
-                ],
-                theme={
-                    "background": "#FFFFFF",
-                    "textColor": "#31333F",
-                    "tooltip": {
-                        "container": {
-                            "background": "#FFFFFF",
-                            "color": "#31333F",
-                        }
-                    }
-                }
-            )
+    with elements("dashboard1"):
+	    layout1 = [dashboard.Item("first_item", 0, 0, 2, 2)]
+	    
+	    DATA = [{ "taste": "fruity", "chardonay": 93, "carmenere": 61, "syrah": 114 },
+            	    { "taste": "bitter", "chardonay": 91, "carmenere": 37, "syrah": 72 },
+            	    { "taste": "heavy", "chardonay": 56, "carmenere": 95, "syrah": 99 },
+                    { "taste": "strong", "chardonay": 64, "carmenere": 90, "syrah": 30 },
+            	    { "taste": "sunny", "chardonay": 119, "carmenere": 94, "syrah": 103 },]
+
+	    with dashboard.Grid(layout1):
+		    with mui.Box:
+			    nivo.Radar(
+				    data=DATA,
+                		    keys=[ "chardonay", "carmenere", "syrah" ],
+				    indexBy="taste",
+				    valueFormat=">-.2f",
+				    margin={ "top": 70, "right": 80, "bottom": 40, "left": 80 },
+				    borderColor={ "from": "color" },        
+				    gridLabelOffset=36,
+				    dotSize=10,
+				    dotColor={ "theme": "background" },
+				    dotBorderWidth=2,
+				    motionConfig="wobbly",
+				    legends=[
+		                    {
+		                        "anchor": "top-left",
+		                        "direction": "column",
+		                        "translateX": -50,
+		                        "translateY": -40,
+		                        "itemWidth": 80,
+		                        "itemHeight": 20,
+		                        "itemTextColor": "#999",
+		                        "symbolSize": 12,
+		                        "symbolShape": "circle",
+		                        "effects": [{"on": "hover","style": {"itemTextColor": "#000"}}]}],
+		                	theme={"background": "#FFFFFF","textColor": "#31333F","tooltip": {"container": {"background": "#FFFFFF","color": "#31333F",}}})
 
 
 
