9b29e2aeaf5f26eaab1b6362c2931f1740fb4beb
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-28 10:23:54 +0100

    Update dev.py

--- dev.py
+++ dev.py
@@ -1184,15 +1184,15 @@ if selected3 == "Elements" :
 					  mui.Toolbar(
 						  mui.IconButton(
 							  size="small",
-							  sx={"borderRadius" : 2, "p" : 2, "aria-label" : "menu"},
-						  ),
+							  sx={"borderRadius" : 2, "p" : 2, "aria-label" : "menu"})
+						  mui.icon.ListIcon(),
 						  mui.Typography(
 							  "Menu",
 							  variant = "h6",
 							  component = "div",
 							  sx = {"flexGrow" : 1},
 						  ),
-						  mui.icon.ListIcon()
+						  
 					  )
 							  
 					
