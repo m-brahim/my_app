7d8f4bee24f1a4cc27ccc06db50e40a9edd1b94b
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-28 16:39:30 +0100

    Update dev.py

--- dev.py
+++ dev.py
@@ -1227,7 +1227,7 @@ if selected3 == "Elements" :
 					  ('Cupcake', 305, 3.7, 67, 4.3),
 					  ('Gingerbread', 356, 16.0, 49, 3.9)]
 				
-				with mui.Box(sx={ flexGrow: 1 }):
+				with mui.Box(sx={ "flexGrow" : 1 }):
 					with mui.AppBar(position="static"):
 						mui.Toolbar(
 							mui.IconButton(
