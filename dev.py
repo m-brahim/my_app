f744698741b9f8baaf962fb12d080fefd4429155
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-28 11:13:23 +0100

    Update dev.py

--- dev.py
+++ dev.py
@@ -1196,7 +1196,7 @@ if selected3 == "Elements" :
 						  ),
 						  mui.Button(
 							  "Login",
-							  sx={ "color" : "success.main" }
+							  sx={ "color" : "error.main" }
 						  )
 						  
 					  )
