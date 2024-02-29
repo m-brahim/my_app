1aaff0e389d8e50cd4d826fb6c662303b46d3b14
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-29 11:16:44 +0100

    Update dev.py

--- dev.py
+++ dev.py
@@ -1231,7 +1231,7 @@ if selected3 == "Elements" :
 				with mui.Box(sx={ "flexGrow" : 1 }) :
 					with mui.AppBar(position="static"):
 							mui.IconButton(
-								mui.icon.MenuIcon,
+								mui.icon.DoubleArrow,
 								size="large",
 								edge="start",
 								aria_label="menu",
