ae0e2d3bab421d3b7f8a04b9a95cf81cf583645b
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-29 17:21:33 +0100

    Update dev.py

--- dev.py
+++ dev.py
@@ -1141,7 +1141,7 @@ if selected3 == "Elements" :
 	                		    keys=[ "chardonay", "carmenere", "syrah" ],
 					    indexBy="taste",
 					    valueFormat=">-.2f",
-					    margin={ "top": 70, "right": 80, "bottom": 40, "left": 80 },
+					    margin={ "top": 70, "right": 80, "bottom": 130, "left": 80 },
 					    borderColor={ "from": "color" },        
 					    gridLabelOffset=36,
 					    dotSize=10,
