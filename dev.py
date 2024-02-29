2794cd93b40fcbdfb7e3f50df9ac45e4b8dae839
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-29 16:46:06 +0100

    Update dev.py

--- dev.py
+++ dev.py
@@ -1119,8 +1119,8 @@ if selected3 == "Elements" :
 		       "fries": 115, "friesColor": "hsl(182, 70%, 50%)", "donut": 30, "donutColor": "hsl(54, 70%, 50%)"},]
 		
 		with dashboard.Grid(layout):
-			with mui.Paper(variant="elevation", sx={ "width" : "100%" }, key="first_item"):
-				with mui.Box( sx={ "flexGrow" : 1 } ) :
+			with mui.Paper("elevation" = 3, key="first_item"):
+				with mui.Box( sx={ "width" : "50%" } ) :
 					with mui.AppBar(position = "static"):
 						with mui.Toolbar :
 							mui.IconButton(
