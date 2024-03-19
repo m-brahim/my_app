e05608c476dcaed0886461b0efe7dcc8b8119eaf
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-03-19 15:33:25 +0100

    Update dev.py

--- dev.py
+++ dev.py
@@ -1067,46 +1067,57 @@ if selected3 == "Tests":
 	st.write(data_filtre)
 
 
+
 	
-    st.header("1. Analyse client")
-    st.subheader("")
-    st.subheader("")
-    
-    # Collecte des données
-    df_table = pd.read_csv(url, delimiter=";").reset_index(drop=True)
-    
-    # Sélectionner les colonnes à afficher dans le DataFrame
-    selected_columns_table = ['Catégorie', 'Date de commande', 'ID client', 'Nom du client', 'Nom du produit', 'Pays/Région', 'Segment', 'Statut des expéditions', 'Ville', 'Quantité', 'Remise', 'Ventes']
+	st.header("1. Analyse client")
+	st.subheader("")
+	st.subheader("")
+	# Collecte des données
+	df_table = pd.read_csv(url, delimiter=";").reset_index(drop=True)
+	    
 
-    # Filtrer le DataFrame avec les colonnes sélectionnées
-    df_filtered = df_table[selected_columns_table].copy()  # Assurez-vous de copier le DataFrame pour éviter les modifications accidentelles
-    
-    # Nettoyer la colonne "Ventes"
-    df_filtered['Ventes'] = df_filtered['Ventes'].str.replace('[^\d]', '', regex=True)
-    df_filtered['Ventes'] = pd.to_numeric(df_filtered['Ventes'], errors='coerce', downcast='integer')
-    
-    df_filtered['Ventes'] = df_filtered['Ventes'].astype(str)
-    df_filtered['Date de commande'] = pd.to_datetime(df_filtered['Date de commande'], format='%d/%m/%Y')
-
-    
-    # Afficher le DataFrame dans Streamlit avec le ProgressColumn pour la colonne "Ventes"
-    st.data_editor(
-    df_filtered,
-    column_config={
-        "Ventes": st.column_config.ProgressColumn(
-            "Volume des ventes",
-            format="%f€",
-            min_value=0,
-            max_value=8000,
-        ),
-        "Date de commande": st.column_config.DateColumn(
-            "Date de commande",
-            format="DD.MM.YYYY",
-            step=1,
-        ),
-    },
-    hide_index=True,
-    )    
+	# Sélectionner les colonnes à afficher dans le DataFrame
+	    
+	selected_columns_table = ['Catégorie', 'Date de commande', 'ID client', 'Nom du client', 'Nom du produit', 'Pays/Région', 'Segment', 'Statut des expéditions', 'Ville', 'Quantité', 'Remise', 'Ventes']
+	
+	    
+	# Filtrer le DataFrame avec les colonnes sélectionnées
+	   
+	df_filtered = df_table[selected_columns_table].copy()  # Assurez-vous de copier le DataFrame pour éviter les modifications accidentelles
+	    
+	   
+	# Nettoyer la colonne "Ventes"
+	   
+	df_filtered['Ventes'] = df_filtered['Ventes'].str.replace('[^\d]', '', regex=True)
+	   
+	df_filtered['Ventes'] = pd.to_numeric(df_filtered['Ventes'], errors='coerce', downcast='integer')
+	    
+	   
+	df_filtered['Ventes'] = df_filtered['Ventes'].astype(str)
+	    
+	df_filtered['Date de commande'] = pd.to_datetime(df_filtered['Date de commande'], format='%d/%m/%Y')
+	
+	    
+	    
+	# Afficher le DataFrame dans Streamlit avec le ProgressColumn pour la colonne "Ventes"
+	    
+	st.data_editor(
+		df_filtered,
+		column_config={
+			"Ventes": st.column_config.ProgressColumn(
+		        "Volume des ventes",
+		        format="%f€",
+		        min_value=0,
+		        max_value=8000,
+		),
+			"Date de commande": st.column_config.DateColumn(
+		        "Date de commande",
+		        format="DD.MM.YYYY",
+		        step=1,
+			),
+		},
+		hide_index=True,
+	)    
 
 
 
