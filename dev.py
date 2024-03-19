258214b76a8032b2657c06d94e1d7ad696255c3e
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-03-19 16:50:11 +0100

    Update dev.py

--- dev.py
+++ dev.py
@@ -1068,11 +1068,11 @@ if selected3 == "Tests":
 	    
 	df_filtered['Date de commande'] = pd.to_datetime(df_filtered['Date de commande'], format='%d/%m/%Y')
 	
-	def ajouter_etoiles(ndc):
+	def ajouter_etoiles(quantite):
 	    if quantite > 10:
-	        return f"{ndc} ⭐"
+	        return f"{quantite} ⭐"
 	    else:
-	        return str(ndc)
+	        return str(quantite)
 
 	df_filtered['Nom du client'] = df_filtered['Nom du client'].apply(ajouter_etoiles)
 
