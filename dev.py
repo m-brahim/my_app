46680c2e0ebd8e99ee0b71239fcbc1e2988dc797
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-03-20 14:56:07 +0100

    Update dev.py

--- dev.py
+++ dev.py
@@ -1083,14 +1083,12 @@ if selected3 == "Tests":
 	categories = df_filtered['Catégorie'].unique().tolist()
 
 	def determine_remise_accorde(remise):
-		if remise not like '0%':
+		if remise == '0%':
 			return False
 		else:
 			return True
 
 	df_filtered['Remise accordé'] = df_filtered['Remise'].apply(determine_remise_accorde)
-
-	data_f = df_filtered[selected_columns]
 
 	
 	
