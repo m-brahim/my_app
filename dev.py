de56f71c0a01582dc3aeb981b3bf899d749757b2
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-03-21 16:00:04 +0100

    Update dev.py

--- dev.py
+++ dev.py
@@ -1042,113 +1042,104 @@ if selected3 == "Tâches":
 
 
 
+def load_data2():
+    if os.path.exists(url):
+        return pd.read_csv(url, delimiter=";").reset_index(drop=True)
+    else:
+        st.error("Le fichier spécifié n'existe pas.")
 
-
+def save_data2(data):
+    data.to_csv(url, sep=';', index=False, encoding='utf-8')
 
 if selected3 == "Tests":
-	st.header("1. Analyse client")
-	st.subheader("")
-	st.subheader("")
+    st.header("1. Analyse client")
+    st.subheader("")
+    st.subheader("")
 
-	df_table = pd.read_csv(url, delimiter=";").reset_index(drop=True)
+    df_table = load_data()
+    if df_table is not None:
+        df_table['Remise accordé'] = True
+        
+        selected_columns_table = ['Catégorie', 'Date de commande', 'ID client', 'Nom du client', 'Nom du produit', 'Pays/Région', 'Segment', 'Statut des expéditions', 'Ville', 'Quantité' , 'Remise accordé' , 'Remise' , 'Ventes']
+    
+        df_filtered = df_table[selected_columns_table].copy()
+           
+        df_filtered['Ventes'] = df_filtered['Ventes'].str.replace('[^\d]', '', regex=True)
+        df_filtered['Ventes'] = pd.to_numeric(df_filtered['Ventes'], errors='coerce', downcast='integer')       
+        df_filtered['Ventes'] = df_filtered['Ventes'].astype(str)
+        
+        df_filtered['Date de commande'] = pd.to_datetime(df_filtered['Date de commande'], format='%d/%m/%Y')
+        
+        def ajouter_etoiles(quantite):
+            if quantite > 10:
+                return f"{quantite} ⭐"
+            else:
+                return str(quantite)
+    
+        df_filtered['Quantité'] = df_filtered['Quantité'].apply(ajouter_etoiles)
+    
+        selected_columns = st.multiselect("Choisir les colonnes à afficher", df_filtered.columns)
+    
+        selection = False
+        
+        if selected_columns is not None:
+            selection = True
+    
+        categories = df_filtered['Catégorie'].unique().tolist()
+    
+        def determine_remise_accorde(remise):
+            if remise == '0%':
+                return True
+            else:
+                return False
+    
+        df_filtered['Remise accordé'] = df_filtered['Remise accordé'].apply(determine_remise_accorde)
+    
+        data_f = df_filtered[selected_columns]
+        
+        if selection:
+            st.data_editor(
+                data_f,
+                column_config={
+                    "Ventes": st.column_config.ProgressColumn(
+                            "Ventes",
+                        format="%f€",
+                            min_value=0,
+                            max_value=8000,
+                ),
+                    "Date de commande": st.column_config.DateColumn(
+                        "Date de commande",
+                        format="DD.MM.YYYY",
+                        step=1,
+                ),
+                    "Catégorie": st.column_config.SelectboxColumn(
+                                "Catégorie",
+                                options=categories
+                        ),
+                },
+                hide_index=True,
+                disabled=["Date de commande"],
+                column_order=('Catégorie', 'Date de commande', 'ID client', 'Nom du client', 'Nom du produit', 'Pays/Région', 'Segment', 'Statut des expéditions', 'Ville', 'Quantité', 'Remise accordé', 'Remise', 'Ventes')
+            )    
+        
+        st.download_button(
+            label="Télécharger",
+            data=convert_df_to_csv(df_filtered),
+            file_name='my_df.csv',
+            mime='text/csv'
+        )
+
+
+
+
+
+
+
+
+
+
 	df2 = "Financial_Data.csv"
 	df_table2 = pd.read_csv(df2, delimiter=";").reset_index(drop=True)
-
-	df_table['Remise accordé'] = True
-	    
-	selected_columns_table = ['Catégorie', 'Date de commande', 'ID client', 'Nom du client', 'Nom du produit', 'Pays/Région', 'Segment', 'Statut des expéditions', 'Ville', 'Quantité' , 'Remise accordé' , 'Remise' , 'Ventes']
-
-	df_filtered = df_table[selected_columns_table].copy()
-	   
-	df_filtered['Ventes'] = df_filtered['Ventes'].str.replace('[^\d]', '', regex=True)
-	df_filtered['Ventes'] = pd.to_numeric(df_filtered['Ventes'], errors='coerce', downcast='integer')	   
-	df_filtered['Ventes'] = df_filtered['Ventes'].astype(str)
-	
-	df_filtered['Date de commande'] = pd.to_datetime(df_filtered['Date de commande'], format='%d/%m/%Y')
-	
-	def ajouter_etoiles(quantite):
-	    if quantite > 10:
-	        return f"{quantite} ⭐"
-	    else:
-	        return str(quantite)
-
-	df_filtered['Quantité'] = df_filtered['Quantité'].apply(ajouter_etoiles)
-
-	selected_columns = st.multiselect("Choisir les colonnes à afficher", df_filtered.columns)
-
-	selection = False
-	
-	if selected_columns is not None :
-		selection = True
-
-	categories = df_filtered['Catégorie'].unique().tolist()
-
-	def determine_remise_accorde(remise):
-		if remise == '0%':
-			return True
-		else:
-			return False
-
-	df_filtered['Remise accordé'] = df_filtered['Remise accordé'].apply(determine_remise_accorde)
-
-	data_f = df_filtered[selected_columns]
-	
-	if selection :
-		st.data_editor(
-			data_f,
-			column_config={
-				"Ventes": st.column_config.ProgressColumn(
-			       		"Ventes",
-					format="%f€",
-			        	min_value=0,
-			        	max_value=8000,
-			),
-				"Date de commande": st.column_config.DateColumn(
-					"Date de commande",
-			        	format="DD.MM.YYYY",
-			        	step=1,
-			),
-				"Catégorie": st.column_config.SelectboxColumn(
-			                "Catégorie",
-                			options=categories
-            		),
-
-
-		},
-			hide_index=True,
-			disabled=["Date de commande"],
-			column_order=('Catégorie', 'Date de commande', 'ID client', 'Nom du client', 'Nom du produit', 'Pays/Région', 'Segment', 'Statut des expéditions', 'Ville', 'Quantité', 'Remise accordé', 'Remise', 'Ventes')
-		)    
-		
-	@st.cache(allow_output_mutation=True)
-	def update_df(new_df):
-		df_filtered[selected_columns] = new_df
-		return df_filtered
-
-	new_data = update_df(data_f)
-
-	def convert_df_to_csv(df):
-		return df.to_csv(sep=';', index=False,encoding='utf-8').encode('utf-8')
-	
-	csv = convert_df_to_csv(df_filtered)
-	st.download_button(
-		label="Télécharger",
-		data=csv,
-		file_name='my_df.csv',
-		mime='text/csv'
-	)
-
-
-
-
-
-
-
-
-
-
-	
-
 	
 	for month in ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']:
 		df_table2[month] = df_table2[month].str.replace(' ', '').astype(int)
