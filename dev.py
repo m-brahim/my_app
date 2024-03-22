70b3a0d523148e89b395c774c5b1d510b02c5323
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-03-22 15:09:58 +0100

    Update dev.py

--- dev.py
+++ dev.py
@@ -988,7 +988,7 @@ if selected3 == "OpenAI":
 
 def load_data():
     if os.path.exists("tasks_data.csv"):
-        return pd.read_csv("tasks_data.csv")
+        return pd.read_csv("tasks_data.csv", sep=";")
     else:
         return pd.DataFrame([
             {"Tâches": "Chargement des données sur Snowflake", "Personnes Assignées": 2, "Durée": "4h", "Statut": "en cours",
