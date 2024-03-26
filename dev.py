90fa7b989aff94cc1585508d9f891a1b663338f8
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-03-26 15:31:14 +0100

    Update dev.py

--- dev.py
+++ dev.py
@@ -1430,9 +1430,11 @@ if selected3 == "Elements" :
 
 if selected3 == "Snowflake":
 	conn = st.connection("snowflake")
-	snow_df = conn.query("select * from COMMANDES")
-	selected_columns_table = ['ID_LIGNE', 'CLIENT_NOM', 'PAYS', 'REGION', 'VILLE', 'CATEGORIE', 'SOUSCATEGORIE', 'PRODUIT_NOM', 'QUANTITE', 'REMISE', 'VENTES', 'BENEFICES']
-	df_filtered = snow_df[selected_columns_table].copy()
-	selected_columns = st.multiselect('Sélectionnez les colonnes à observer', df_filtered.columns)
-	st.data_editor(selected_columns)
+        snow_df = conn.query("select * from COMMANDES")
+        selected_columns_table = ['ID_LIGNE', 'CLIENT_NOM', 'PAYS', 'REGION', 'VILLE', 'CATEGORIE', 'SOUSCATEGORIE', 'PRODUIT_NOM', 'QUANTITE', 'REMISE', 'VENTES', 'BENEFICES']
+        df_filtered = snow_df[selected_columns_table].copy()
+    
+        # Afficher les colonnes sélectionnées dans l'éditeur de données
+        selected_columns = st.multiselect('Sélectionnez les colonnes à observer', df_filtered.columns)
+        st.dataframe(df_filtered[selected_columns])
 	
